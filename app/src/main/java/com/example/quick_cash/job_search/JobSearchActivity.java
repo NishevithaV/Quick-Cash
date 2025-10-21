d0c3559216842b6768c253576c9339fd3c039430
Emmanuel Yusuff <em321000@dal.ca>
2025-10-20 23:07:55 -0300

    Initial UI for job searching with button, spinner, and results text view

--- app/src/main/java/com/example/quick_cash/job_search/JobSearchActivity.java
+++ app/src/main/java/com/example/quick_cash/job_search/JobSearchActivity.java
@@ -2,6 +2,7 @@ package com.example.quick_cash.job_search;
 
 import android.os.Bundle;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,14 +12,20 @@ import com.example.quick_cash.R;
 public class JobSearchActivity extends AppCompatActivity {
 
     EditText userSearch;
+    TextView resultsView;
+    Spinner categorySelector;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.setContentView(R.layout.activity_job_list);
+        initUI();
     }
 
     protected void initUI() {
         this.userSearch = findViewById(R.id.userSearch);
+        this.resultsView = findViewById(R.id.resultsView);
+        this.categorySelector = findViewById(R.id.catSelect);
     }
 }
---
+++ app/src/test/java/com/example/quick_cash/job_search/JUnitTest.java
@@ -0,0 +1,11 @@
+package com.example.quick_cash.job_search;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import java.util.*;
+
+
+public class JUnitTest {
+
+}
