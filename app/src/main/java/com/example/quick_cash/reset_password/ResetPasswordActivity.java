c2e524e96356af1598488535accf2612687cae45
Eduardo Rojas Gonzalez <ed775324@dal.ca>
2025-10-22 13:00:09 -0300

    feat/ui-reset-password: add email input + button (UI only)

---
+++ app/src/main/java/com/example/quick_cash/reset_password/ResetPasswordActivity.java
@@ -0,0 +1,15 @@
+package com.example.quick_cash.reset_password;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.quick_cash.R;
+
+public class ResetPasswordActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.setContentView(R.layout.activity_reset_password);
+    }
+}
