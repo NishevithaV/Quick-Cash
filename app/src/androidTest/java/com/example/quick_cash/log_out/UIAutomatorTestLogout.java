45b91ecaeff22d9c937537fac6f4a6ed93b9db62
Jiarui Gao <jr853514@dal.ca>
2025-10-29 22:43:41 -0300

    created UIAutomator test setup and test to check if settings page loads (LFTG) - J.G

--- app/src/androidTest/java/com/example/quick_cash/log_out/UIAutomatorTestLogout.java
+++ app/src/androidTest/java/com/example/quick_cash/log_out/UIAutomatorTestLogout.java
@@ -2,17 +2,46 @@ package com.example.quick_cash.log_out;
 
 import static org.junit.Assert.assertTrue;
 
+import android.content.Context;
+import android.content.Intent;
+
+import androidx.test.core.app.ApplicationProvider;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.uiautomator.By;
 import androidx.test.uiautomator.UiDevice;
 import androidx.test.uiautomator.UiObject;
-import androidx.test.uiautomator.UiObject2;
-import androidx.test.uiautomator.UiObjectNotFoundException;
 import androidx.test.uiautomator.UiSelector;
 import androidx.test.uiautomator.Until;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 
 @RunWith(AndroidJUnit4.class)
 public class UIAutomatorTestLogout {
+
+    private static final int LAUNCH_TIMEOUT = 5000;
+    final String launcherPackageName = "com.example.quick_cash.log_out";
+    private UiDevice device;
+
+    @Before
+    public void setup() {
+        device = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation());
+        Context context = ApplicationProvider.getApplicationContext();
+        Intent launcherIntent = context.getPackageManager().getLaunchIntentForPackage(launcherPackageName);
+        Assert.assertNotNull(launcherIntent);
+        launcherIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        context.startActivity(launcherIntent);
+        device.wait(Until.hasObject(By.pkg(launcherPackageName).depth(0)), LAUNCH_TIMEOUT);
+    }
+
+    @Test
+    public void checkIfSettingsPageIsVisible() {
+        UiObject LogoutButton = device.findObject(new UiSelector().text("Logout"));
+        assertTrue(LogoutButton.exists());
+        UiObject textView = device.findObject(new UiSelector().textContains("Settings"));
+        assertTrue(textView.exists());
+    }
 }
