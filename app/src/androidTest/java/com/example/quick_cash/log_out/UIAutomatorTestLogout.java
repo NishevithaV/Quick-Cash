92977288bba620c02e2a451d7b4596873be0c2ba
Jiarui Gao <jr853514@dal.ca>
2025-10-29 23:01:29 -0300

    created UIAutomator test to check if cancelling the logout works (LFTG) - J.G

--- app/src/androidTest/java/com/example/quick_cash/log_out/UIAutomatorTestLogout.java
+++ app/src/androidTest/java/com/example/quick_cash/log_out/UIAutomatorTestLogout.java
@@ -11,6 +11,7 @@ import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.uiautomator.By;
 import androidx.test.uiautomator.UiDevice;
 import androidx.test.uiautomator.UiObject;
+import androidx.test.uiautomator.UiObjectNotFoundException;
 import androidx.test.uiautomator.UiSelector;
 import androidx.test.uiautomator.Until;
 
@@ -43,5 +44,17 @@ public class UIAutomatorTestLogout {
         assertTrue(LogoutButton.exists());
         UiObject textView = device.findObject(new UiSelector().textContains("Settings"));
         assertTrue(textView.exists());
+    }
+
+    @Test
+    public void checkIfCancelWorks() throws UiObjectNotFoundException {
+        UiObject LogoutButton = device.findObject(new UiSelector().text("Logout"));
+        LogoutButton.click();
+        UiObject CancelButton = device.findObject(new UiSelector().text("Cancel"));
+        CancelButton.click();
+
+        UiObject textView = device.findObject(new UiSelector().textContains("Settings"));
+        assertTrue(textView.exists());
+        assertTrue(LogoutButton.exists());
     }
 }
