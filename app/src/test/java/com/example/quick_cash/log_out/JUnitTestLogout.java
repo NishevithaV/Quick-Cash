5fd869b5326a13e88ecb3017194a32ff110dd4e3
Jiarui Gao <jr853514@dal.ca>
2025-10-30 00:53:47 -0300

    created the idea for my JUnit test as a comment as i could not figure out mokito, will ask for help (LFTG) - J.G

--- app/src/test/java/com/example/quick_cash/log_out/JUnitTestLogout.java
+++ app/src/test/java/com/example/quick_cash/log_out/JUnitTestLogout.java
@@ -1,11 +1,15 @@
 package com.example.quick_cash.log_out;
 
+import static org.mockito.Mockito.*;
 import org.junit.Test;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class JUnitTestLogout {
 
     @Test
     public void checkIfLoggedOut() {
+        //could not figure out how to get mockito working, gonna leave some code here
 
+        //create new page, logout and then check if it logged out with verify().signout();
     }
 }
