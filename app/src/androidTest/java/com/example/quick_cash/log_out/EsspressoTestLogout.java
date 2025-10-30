032109fe1f28858535112d69bab55dbb87671461
Jiarui Gao <jr853514@dal.ca>
2025-10-30 18:07:41 -0300

    updated failing esspresso tests (LFTG) - J.G

--- app/src/androidTest/java/com/example/quick_cash/log_out/EsspressoTestLogout.java
+++ app/src/androidTest/java/com/example/quick_cash/log_out/EsspressoTestLogout.java
@@ -5,13 +5,11 @@ import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
-import static org.hamcrest.CoreMatchers.is;
 
 import androidx.test.core.app.ActivityScenario;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
-import static org.hamcrest.Matchers.allOf;
-import static org.junit.Assert.assertEquals;
+import com.example.quick_cash.R;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -32,21 +30,21 @@ public class EsspressoTestLogout {
 
     @Test
     public void checkIfLogoutButtonWorks() {
-        onView(withId(R.id.LogoutButton)).perform(click());
-        onView(withId(R.id.textView)).check(matches(withText("Are you sure you would like to logout?")));
+        onView(withId(R.id.logoutButton)).perform(click());
+        onView(withId(R.id.textView)).check(matches(withText("Confirm Logout")));
     }
 
     @Test
     public void checkIfCancelButtonWorks() {
-        onView(withId(R.id.LogoutButton)).perform(click());
-        onView(withId(R.id.CancelButton)).perform(click());
+        onView(withId(R.id.logoutButton)).perform(click());
+        onView(withText("Cancel")).perform(click());
         onView(withId(R.id.textView)).check(matches(withText("Settings")));
     }
 
     @Test
     public void checkIfConfirmButtonWorks() {
-        onView(withId(R.id.LogoutButton)).perform(click());
-        onView(withId(R.id.ConfirmButton)).perform(click());
+        onView(withId(R.id.logoutButton)).perform(click());
+        onView(withText("Yes")).perform(click());
         onView(withId(R.id.textView)).check(matches(withText("Login")));
     }
 }
