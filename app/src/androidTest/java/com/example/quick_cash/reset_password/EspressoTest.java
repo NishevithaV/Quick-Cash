ea46d0ff44aeb060f1fb31fea4dac8d4fb997946
Eduardo Rojas Gonzalez <ed775324@dal.ca>
2025-10-31 17:02:32 -0300

    test: updated espresso tests for reworked design based on new info (ER-US5)

--- app/src/androidTest/java/com/example/quick_cash/reset_password/EspressoTest.java
+++ app/src/androidTest/java/com/example/quick_cash/reset_password/EspressoTest.java
@@ -8,33 +8,55 @@ import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
-import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import static org.hamcrest.CoreMatchers.not;
+
+import androidx.test.core.app.ActivityScenario;
+import androidx.test.espresso.Espresso;
+import androidx.test.espresso.IdlingRegistry;
+import androidx.test.espresso.idling.CountingIdlingResource;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import com.example.quick_cash.R;
 
-import org.junit.Rule;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 @RunWith(AndroidJUnit4.class)
 public class EspressoTest {
 
-    @Rule
-    public ActivityScenarioRule<ResetPasswordActivity> rule =
-            new ActivityScenarioRule<>(ResetPasswordActivity.class);
+    public ActivityScenario<ResetPasswordActivity> activityScenario;
 
-    @Test
-    public void testSuccesfulToast() {
-        onView(withId(R.id.emailInputID)).perform(typeText("alex@gmail.com"), closeSoftKeyboard());
-        onView(withId(R.id.resetButtonID)).perform(click());
-        onView(withId(R.id.statusTextID)).check(matches(withText("reset email sent!")));
+    @Before
+    public void setup() {
+        activityScenario = ActivityScenario.launch(ResetPasswordActivity.class);
+        activityScenario.onActivity(activity -> {
+        });
     }
 
     @Test
-    public void testErrorToast() {
-        onView(withId(R.id.emailInputID)).perform(typeText("alex.gmail.com"), closeSoftKeyboard());
+    public void testValidEmail() {
+        onView(withId(R.id.emailInputID)).perform(typeText("thisemaildoesnotexitindb@validemail.com"), closeSoftKeyboard());
         onView(withId(R.id.resetButtonID)).perform(click());
-        onView(withId(R.id.statusTextID)).check(matches(withText("invalid email")));
+        onView(withId(R.id.statusTextID)).check(matches(not(withText(R.string.INVALID_EMAIL))));
+    }
+
+    @Test
+    public void testInvalidEmail() {
+        onView(withId(R.id.emailInputID)).perform(typeText("bademail.com"), closeSoftKeyboard());
+        onView(withId(R.id.resetButtonID)).perform(click());
+        onView(withId(R.id.statusTextID)).check(matches(withText(R.string.INVALID_EMAIL)));
+    }
+
+    @Test
+    public void testSuccessfulResetSend() {
+
+        onView(withId(R.id.emailInputID)).perform(typeText("test-user@bigmoney.com"), closeSoftKeyboard());
+        onView(withId(R.id.resetButtonID)).perform(click());
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+        }
+        onView(withId(R.id.statusTextID)).check(matches(withText(R.string.RESET_SEND_SUCCESSFUL)));
     }
 }
