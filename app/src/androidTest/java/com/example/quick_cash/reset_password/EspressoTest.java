2b12c4114b7fd7f0085ca563183946251932573a
Eduardo Rojas Gonzalez <ed775324@dal.ca>
2025-10-31 22:38:35 -0300

    refactor/test: refactored string values to variables in ui automator test and included testing for going to login page after succesful reset(ER-US5)

--- app/src/androidTest/java/com/example/quick_cash/reset_password/EspressoTest.java
+++ app/src/androidTest/java/com/example/quick_cash/reset_password/EspressoTest.java
@@ -5,15 +5,13 @@ import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
 import static org.hamcrest.CoreMatchers.not;
 
 import androidx.test.core.app.ActivityScenario;
-import androidx.test.espresso.Espresso;
-import androidx.test.espresso.IdlingRegistry;
-import androidx.test.espresso.idling.CountingIdlingResource;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import com.example.quick_cash.R;
@@ -36,27 +34,29 @@ public class EspressoTest {
 
     @Test
     public void testValidEmail() {
-        onView(withId(R.id.emailInputID)).perform(typeText("thisemaildoesnotexitindb@validemail.com"), closeSoftKeyboard());
+        onView(withId(R.id.resetEmailInputID)).perform(typeText("thisemaildoesnotexitindb@validemail.com"), closeSoftKeyboard());
         onView(withId(R.id.resetButtonID)).perform(click());
-        onView(withId(R.id.statusTextID)).check(matches(not(withText(R.string.INVALID_EMAIL))));
+        onView(withId(R.id.resetPsswdStatusTextID)).check(matches(not(withText(R.string.INVALID_EMAIL))));
     }
 
     @Test
     public void testInvalidEmail() {
-        onView(withId(R.id.emailInputID)).perform(typeText("bademail.com"), closeSoftKeyboard());
+        onView(withId(R.id.resetEmailInputID)).perform(typeText("bademail.com"), closeSoftKeyboard());
         onView(withId(R.id.resetButtonID)).perform(click());
-        onView(withId(R.id.statusTextID)).check(matches(withText(R.string.INVALID_EMAIL)));
+        onView(withId(R.id.resetPsswdStatusTextID)).check(matches(withText(R.string.INVALID_EMAIL)));
+        onView(withId(R.id.resetTologinLinkID)).check(matches(not(isDisplayed())));
     }
 
     @Test
     public void testSuccessfulResetSend() {
 
-        onView(withId(R.id.emailInputID)).perform(typeText("test-user@bigmoney.com"), closeSoftKeyboard());
+        onView(withId(R.id.resetEmailInputID)).perform(typeText("test-user@bigmoney.com"), closeSoftKeyboard());
         onView(withId(R.id.resetButtonID)).perform(click());
         try {
             Thread.sleep(2000);
         } catch (InterruptedException e) {
         }
-        onView(withId(R.id.statusTextID)).check(matches(withText(R.string.RESET_SEND_SUCCESSFUL)));
+        onView(withId(R.id.resetPsswdStatusTextID)).check(matches(withText(R.string.RESET_SEND_SUCCESSFUL)));
+        onView(withId(R.id.resetTologinLinkID)).check(matches(isDisplayed()));
     }
 }
--- app/src/androidTest/java/com/example/quick_cash/reset_password/UIAutomatorTest.java
+++ app/src/androidTest/java/com/example/quick_cash/reset_password/UIAutomatorTest.java
@@ -10,7 +10,9 @@ import androidx.test.core.app.ApplicationProvider;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.uiautomator.By;
 import androidx.test.uiautomator.UiDevice;
+import androidx.test.uiautomator.UiObject;
 import androidx.test.uiautomator.UiObject2;
+import androidx.test.uiautomator.UiSelector;
 import androidx.test.uiautomator.Until;
 
 import org.junit.Assert;
@@ -22,41 +24,72 @@ public class UIAutomatorTest {
     final String launcherPackageName = "com.example.quick_cash";
     private UiDevice device;
 
+    private final String resetBtnID = "resetButtonID";
+    private final String loginLink = "resetTologinLinkID";
+    private final String emailInpID = "resetEmailInputID";
+    private final String statusTxtID = "resetPsswdStatusTextID";
+
     @Before
     public void setup() {
         device = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation());
         Context context = ApplicationProvider.getApplicationContext();
-        Intent launcherIntent = context.getPackageManager().getLaunchIntentForPackage(launcherPackageName);
-        Assert.assertNotNull(launcherIntent);
-        launcherIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+
+        Intent launcherIntent = new Intent();
+        launcherIntent.setClassName(
+                launcherPackageName,
+                launcherPackageName+".reset_password.ResetPasswordActivity"
+        );
+        launcherIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+
         context.startActivity(launcherIntent);
         device.wait(Until.hasObject(By.pkg(launcherPackageName).depth(0)), LAUNCH_TIMEOUT);
     }
 
     @Test
     public void checkIfResetPasswordPageIsVisible() {
-        UiObject2 resetBtn = device.findObject(By.res(launcherPackageName, "resetButtonID"));
+        UiObject2 resetBtn = device.findObject(By.res(launcherPackageName, resetBtnID));
         assertNotNull(resetBtn);
-        UiObject2 emailBox = device.findObject(By.res(launcherPackageName, "emailInputID"));
+        UiObject2 emailBox = device.findObject(By.res(launcherPackageName, emailInpID));
         assertNotNull(emailBox);
     }
 
     @Test
     public void testResetButtonWithInvalidEmail() {
-        device.findObject(By.res(launcherPackageName, "emailInputID")).setText("bademail.com");
-        device.findObject(By.res(launcherPackageName, "resetButtonID")).click();
+        device.findObject(By.res(launcherPackageName, emailInpID)).setText("bademail.com");
+        device.findObject(By.res(launcherPackageName, resetBtnID)).click();
         device.waitForIdle();
-        assertTrue(device.findObject(By.res(launcherPackageName, "statusTextID")).getText().contains("Enter enter a Valid Email"));
+        assertTrue(device.findObject(By.res(launcherPackageName, statusTxtID)).getText().contains("Enter enter a Valid Email"));
     }
 
     @Test
     public void testResetButtonWithValidEmail() throws InterruptedException {
-        String testEmail = "emiyusuffe@gmail.com";
-        device.findObject(By.res(launcherPackageName, "emailInputID")).setText(testEmail);
-        device.findObject(By.res(launcherPackageName, "resetButtonID")).click();
+        String testEmail = "test-user@bigmoney.com";
+        device.findObject(By.res(launcherPackageName, emailInpID)).setText(testEmail);
+        device.findObject(By.res(launcherPackageName, resetBtnID)).click();
         device.waitForIdle();
-        device.wait(Until.hasObject(By.res(launcherPackageName, "statusTextID")), 5000);
-        String status = device.findObject(By.res(launcherPackageName, "statusTextID")).getText();
+        device.wait(Until.hasObject(By.res(launcherPackageName, statusTxtID)), 5000);
+        String status = device.findObject(By.res(launcherPackageName, statusTxtID)).getText();
+        String login = device.findObject(By.res(launcherPackageName, loginLink)).getText();
         assertTrue(status.contains("email sent"));
+        assertTrue(login.contains("Go To Login"));
+    }
+
+    @Test
+    public void testGoToLoginAfterReset() {
+        String testEmail = "test-user@bigmoney.com";
+        device.findObject(By.res(launcherPackageName, emailInpID)).setText(testEmail);
+        device.findObject(By.res(launcherPackageName, resetBtnID)).click();
+        device.waitForIdle();
+        device.wait(Until.hasObject(By.res(launcherPackageName, statusTxtID)), 5000);
+        UiObject2 status = device.findObject(By.res(launcherPackageName, statusTxtID));
+        UiObject2 loginBtn = device.findObject(By.res(launcherPackageName, loginLink));
+        assertTrue(status.getText().contains("email sent"));
+        assertTrue(loginBtn.getText().contains("Go To Login"));
+
+        loginBtn.click();
+        UiObject loginHeader = device.findObject(new UiSelector().textContains("Welcome Back"));
+        loginHeader.exists();
+        UiObject loginPageBtn = device.findObject(new UiSelector().textContains("Login"));
+        loginPageBtn.exists();
     }
 }
