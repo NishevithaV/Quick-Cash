c87fa4563edd788bd750d708f5935cabbccbd4ad
Eduardo Rojas <ed775324@dal.ca>
2025-11-29 14:36:49 -0400

    test: created failing JUnit test for submit application logic (ER – US-10)

---
+++ app/src/test/java/com/example/quick_cash/employee/SubmitApplicationJUnitTest.java
@@ -0,0 +1,40 @@
+package com.example.quick_cash.employee;
+import static org.junit.Assert.*;
+import com.example.quick_cash.utils.FirebaseCRUD.Applications;
+import com.example.quick_cash.utils.FirebaseCRUD.Users;
+import com.example.quick_cash.utils.SubmitApplicationHandler;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+
+public class SubmitApplicationJUnitTest {
+
+    String jobID;
+    String jobID2;
+    String uID;
+    SubmitApplicationHandler appHandler;
+
+    @Before
+    public void setUp() {
+        uID = "testUserID";
+        jobID = "testJobID";
+        jobID2 = "testJobIDAlreadyApplied";
+        Users mockUsers = Mockito.mock(Users.class);
+        Applications mockApps = Mockito.mock(Applications.class);
+        appHandler = new SubmitApplicationHandler(mockUsers, mockApps);
+    }
+
+    @Test
+    public void testSaveApplication() {
+        final String[] result = new String[1];
+        appHandler.submitApp(uID, jobID2, "My cover letter", new SubmitApplicationHandler.SubmitCallback() {
+            @Override
+            public void onCallback(String msg) {
+                result[0] = msg;
+            }
+        });
+        assertEquals(SubmitApplicationHandler.APPLICATION_SUCCESS, result[0]); // AT-5: should be marked as applied
+    }
+}
