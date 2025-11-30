e65e03950b46819b47a15bb06601104f7363489e
Eduardo Rojas <ed775324@dal.ca>
2025-11-30 01:37:34 -0400

    test: (AT-4) created espresso test for check succesful submission toast. tests failing (ER – US-10)

--- app/src/androidTest/java/com/example/quick_cash/views/employee/SubmitApplicationEspressoTest.java
+++ app/src/androidTest/java/com/example/quick_cash/views/employee/SubmitApplicationEspressoTest.java
@@ -1,30 +1,45 @@
 package com.example.quick_cash.views.employee;
 
+import androidx.test.core.app.ActivityScenario;
 import androidx.test.core.app.ApplicationProvider;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import com.example.quick_cash.R;
 import com.example.quick_cash.views.employee.SubmitApplicationActivity;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.clearText;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
+
+import static org.junit.Assert.assertEquals;
 
 import android.content.Context;
 import android.content.Intent;
 
 @RunWith(AndroidJUnit4.class)
 public class SubmitApplicationEspressoTest {
-
-    @Rule
-    public ActivityScenarioRule<SubmitApplicationActivity> rule =
-            new ActivityScenarioRule<>(createIntent());
+    private ActivityScenario<SubmitApplicationActivity> scenario;
+    @Before
+    public void setup() {
+        FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+        if (currentUser != null) {
+            FirebaseAuth.getInstance().signOut();
+        }
+        scenario = ActivityScenario.launch(createIntent());
+    }
 
     private Intent createIntent() {
         Context ctx = ApplicationProvider.getApplicationContext();
@@ -37,6 +52,16 @@ public class SubmitApplicationEspressoTest {
     public void testAllViewsVisible() {
         onView(withId(R.id.cvrLtrInput)).check(matches(isDisplayed()));
         onView(withId(R.id.submitLtrBtn)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void testSuccessfulSubmissionToast() {
+        onView(withId(R.id.cvrLtrInput)).perform(clearText(), typeText("Cover letter"), closeSoftKeyboard());
+        onView(withId(R.id.submitLtrBtn)).perform(click());
+
+        scenario.onActivity(activity -> {
+            assertEquals("Application Submitted", activity.lastToastMessage);
+        });
     }
 }
 
